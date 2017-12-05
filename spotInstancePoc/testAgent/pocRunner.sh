4074de1baa0136f28413a1fa5384c1659e6b3ede
Pranab Padhy <padhypranabkumar@gmail.com.com>
2017-12-05 11:39:41 +0530

    change in pocRunner

--- spotInstancePoc/testAgent/pocRunner.sh
+++ spotInstancePoc/testAgent/pocRunner.sh
@@ -40,4 +40,11 @@ yes | sdkmanager --sdk_root=/root/android-sdk/ --licenses
 
 echo no | avdmanager -s --clear-cache create avd -n Nexus -f -k "system-images;$API;google_apis;x86"
 
+# Detect ip and forward ADB ports outside to outside interface
+ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
+socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
+socat tcp-listen:80,bind=$ip,fork tcp:127.0.0.1:80 &
+socat tcp-listen:443,bind=$ip,fork tcp:127.0.0.1:443 &
+
 /bin/bash
